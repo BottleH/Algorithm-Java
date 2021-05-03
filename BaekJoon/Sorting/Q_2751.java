5b7a66d93508215d321ba355e0f44eae8a6a71c1
KimByeongHwan <guys1017@korea.ac.kr>
2021-05-03 21:09:31 +0900

    update.Sorting-q2751

---
+++ BaekJoon/Sorting/Q_2751.java
@@ -0,0 +1,27 @@
+// 2750의 방법으로는 시간초과가 뜸.
+package Sorting;
+
+import java.util.*;
+
+public class Q_2751 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = sc.nextInt();
+
+        ArrayList<Integer> ls = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            ls.add(sc.nextInt());
+        }
+
+        Collections.sort(ls);
+
+        for (int val : ls) {
+            sb.append(val).append("\n");
+        }
+        System.out.println(sb);
+    }
+}

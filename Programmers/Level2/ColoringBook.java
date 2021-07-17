d452ad07d8d5bc3c1462f6a7d046b43cf3841230
BottleH <guys1017@korea.ac.kr>
2021-07-17 23:57:19 +0900

    add ColoringBook

---
+++ Programmers/Level2/ColoringBook.java
@@ -0,0 +1,57 @@
+package Level2;
+
+public class ColoringBook {
+
+    static int areaCnt = 0;
+
+    public int[] solution(int m, int n, int[][] picture) {
+        int numberOfArea = 0;
+        int maxSizeOfOneArea = 0;
+        boolean[][] check = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (picture[i][j] != 0 && !check[i][j]) {
+                    numberOfArea++;
+                    dfs(i, j, picture, check);
+                }
+
+                if (areaCnt > maxSizeOfOneArea) {
+                    maxSizeOfOneArea = areaCnt;
+                }
+                areaCnt = 0;
+            }
+        }
+
+        int[] answer = new int[2];
+        answer[0] = numberOfArea;
+        answer[1] = maxSizeOfOneArea;
+        return answer;
+    }
+
+    public static void dfs(int x, int y, int[][] picture, boolean[][] check) {
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        if (check[x][y]) {
+            return;
+        }
+
+        check[x][y] = true;
+        areaCnt++;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= picture.length || ny < 0 || ny >= picture[0].length) {
+                continue;
+            }
+
+            if (picture[x][y] == picture[nx][ny] && !check[nx][ny]) {
+                dfs(nx, ny, picture, check);
+            }
+        }
+    }
+}

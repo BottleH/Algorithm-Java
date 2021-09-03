1a9e677411af6f900c06dac959c2004ed2d8e91c
KimByeongHwan <guys1017@korea.ac.kr>
2021-09-03 21:03:42 +0900

    dfs 이용방법 추가

--- Programmers/Weekly_Challenge/Q5.java
+++ Programmers/Weekly_Challenge/Q5.java
@@ -1,7 +1,30 @@
 package Weekly_Challenge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Q5 {
+    List<String> dictionary = new ArrayList<>();
+    String aeiou = "AEIOU";
+
+    private void dfs(String str, int len) {
+        if(len > 5) {
+            return;
+        }
+
+        dictionary.add(str);
+
+        for(int i = 0; i < 5; i++){
+            dfs(str + aeiou.charAt(i), len + 1);
+        }
+    }
+
     public int solution(String word) {
+        dfs("", 0);
+        return dictionary.indexOf(word);
+    }
+
+    public int solution2(String word) {
         int answer = 0;
         int interval = 781; // 5자리일 때 최초 간격
         char[] vowel = {'A', 'E', 'I', 'O', 'U'};

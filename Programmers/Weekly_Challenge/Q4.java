f8242360b59e905f30c1da1afebe05dc30568e25
BottleH <guys1017@korea.ac.kr>
2021-10-08 23:36:13 +0900

    fix(week_4): 수정

--- Programmers/Weekly_Challenge/Q4.java
+++ Programmers/Weekly_Challenge/Q4.java
@@ -4,31 +4,37 @@ import java.util.*;
 
 public class Q4 {
     public String solution(String[] table, String[] languages, int[] preference) {
-        Map<Integer, String> scoreMap = new HashMap<>();
+        Map<String, Integer> scoreMap = new HashMap<>();
+
+        for (int i = 0; i < languages.length; i++) {
+            scoreMap.put(languages[i], preference[i]);
+        }
+
+        int maxNumber = 0;
+        String answer = "";
 
         for (String value : table) {
             String[] s = value.split(" ");
             int score = 0;
-            for (int j = 0; j < languages.length; j++) {
-                for (int k = 1; k < s.length; k++) {
-                    if (!s[k].equals(languages[j])) {
-                        continue;
-                    }
-                    score += preference[j] * (6 - k);
+            for (int j = 1; j < s.length; j++) {
+                if (!scoreMap.containsKey(s[j])) {
+                    continue;
                 }
+                score += scoreMap.get(s[j]) * (6 - j);
             }
-            if (scoreMap.getOrDefault(score, null) != null) {
-                String[] compareStr = new String[2];
-                compareStr[0] = scoreMap.get(score);
-                compareStr[1] = s[0];
-                Arrays.sort(compareStr);
-                scoreMap.put(score, compareStr[0]);
+
+            if (maxNumber < score) {
+                maxNumber = score;
+                answer = s[0];
                 continue;
             }
-            scoreMap.put(score, s[0]);
+            if (maxNumber == score) {
+                if (answer.compareTo(s[0]) > 0) {
+                    answer = s[0];
+                }
+            }
         }
 
-        int maxScore = Collections.max(scoreMap.keySet());
-        return scoreMap.get(maxScore);
+        return answer;
     }
 }

35d3f8d6f127aa32910e6ece3ad31d7147a4e14e
BottleH <guys1017@korea.ac.kr>
2021-10-29 16:32:44 +0900

    feat(1018): 브루트포스 예제 추가

---
+++ BaekJoon/bruteForcing/Q_1018.java
@@ -0,0 +1,64 @@
+package bruteForcing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q_1018 {
+
+    public static boolean[][] matrix;
+    public static int min = 64;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        matrix = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                if (str.charAt(j) == 'W') {
+                    matrix[i][j] = true;
+                } else {
+                    matrix[i][j] = false;
+                }
+            }
+        }
+
+        int N_row = N - 7;
+        int M_col = M - 7;
+
+        for (int i = 0; i < N_row; i++) {
+            for (int j = 0; j < M_col; j++) {
+                find(i, j);
+            }
+        }
+        System.out.println(min);
+    }
+
+    public static void find(int x, int y) {
+        int end_x = x + 8;
+        int end_y = y + 8;
+        int count = 0;
+
+        boolean currentValue = matrix[x][y];
+
+        for (int i = x; i < end_x; i++) {
+            for (int j = y; j < end_y; j++) {
+                if (matrix[i][j] != currentValue) {
+                    count++;
+                }
+                currentValue = !currentValue;
+            }
+            currentValue = !currentValue;
+        }
+        count = Math.min(count, min - count);
+        min = Math.min(min, count);
+    }
+}

8080c21221eb20a8a88bc229b32ffb0757df99d3
bottleh <guys0217@gmail.com>
2022-08-21 20:54:54 +0900

    [add] Q_1436

---
+++ BaekJoon/bruteForcing/Q_1436.java
@@ -0,0 +1,33 @@
+package bruteForcing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q_1436 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        System.out.print(findMinDoomNumber(n));
+    }
+
+    private static int findMinDoomNumber(int n) {
+        int doomNumber = 666;
+        int count = 1;
+
+        while (count != n) {
+            doomNumber++;
+
+            if (Integer.toString(doomNumber).contains("666")) {
+                count++;
+            }
+        }
+
+        return doomNumber;
+    }
+}
